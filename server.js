5d04b5ed22a016a73a406a2d492363eb28200377
batraccc <batracmatija4@gmail.com>
2025-11-07 21:39:51 +0100

    Add currency converter

--- server.js
+++ server.js
@@ -18,6 +18,21 @@ app.get("/", async (req, res) => {
     }
 })
 
+app.post("/calculate", async (req, res) => {
+    try{
+        const from_currency = (req.body.from).toUpperCase();
+        const first_response = await axios.get("https://api.frankfurter.dev/v1/latest?symbols=USD,EUR,JPY,GBP,AUD,CAD,CHF,MXN,HKD,NZD");
+        const second_response = await axios.get(`https://api.frankfurter.dev/v1/latest?base=${from_currency}`);
+        const to_currency = (req.body.to).toUpperCase();
+        const from_amount = req.body.from_amount;
+        const total = from_amount * second_response.data.rates[`${to_currency}`];
+        res.render("index.ejs", {content: first_response.data, total:total});
+    } catch (error){
+        console.log("Error");
+        res.status(500);
+    }
+})
+
 app.listen(port, (req, res) => {
     console.log(`Listening at port ${port}`);
 })
